c5a05c5ca94acd0d9d208dca99a470d093c7bb13
kyl-dart230 <123567860+kyl-dart230@users.noreply.github.com>
2023-03-30 20:06:27 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let Sport = 0;
+let Net =  0
+let r=10;
+
+let x,y;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  x=width/2;
+  y=height;
+}
+
+function draw() {
+  background(96, 23, 131);
+  angleMode(DEGREES)
+  fill(0)
+  rect( 00, 300, 400, 190)
+  
+  
+  //Basketball
+  noStroke()
+  fill(0,0,0)
+
+  ellipse(x,y, 100, 100);
+  y=y-3;
+  if(y<0){y=height;
+         }
+  
+  
+  fill(89,255,0)
+rect(160, 170, 45, 30)
+  rect( 200,170,45,30) 
+  fill(255,34,0)
+  rect(200,170,5,20)
+  rect(195,170,5,20)
+  noFill()
+  stroke(0)
+  rect(190,200,25,80);
+  ellipse(200, 150, 100, 50)
+  
+  
+  rect(100,100,100,100) 
+  rect(200,100,100,100)
+  fill(252,156,20)
+  
+  
+  stroke(0)
+  strokeWeight(4)
+  ellipse(100,100)
+  
+  
+  
+}
